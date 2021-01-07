66b26b0ee91492f294a3b5f2f520d33907b81341
Kübra Bilgin <bilgin.kbra@gmail.com>
2021-01-07 17:55:47 +0300

    DbContext and entity created, injected in startup

---
+++ Catalog.API/Entities/Product.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Entities
+{
+    public class Product
+    {
+        //Mongo Entity- add mongodb driver
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        //[BsonElement("Name")]
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Summary { get; set; }
+        public string Decription { get; set; }
+        public string ImageFile { get; set; }
+        public decimal Price { get; set; }
+    }
+}
--- Catalog.API/Startup.cs
+++ Catalog.API/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Catalog.API.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -31,6 +32,10 @@ namespace Catalog.API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog.API", Version = "v1" });
             });
+
+            services.Configure<CatalogDatabseSettings>(Configuration.GetSection(nameof(CatalogDatabseSettings)));
+            services.AddSingleton<ICatalogDatabaseSettings, CatalogDatabseSettings>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
