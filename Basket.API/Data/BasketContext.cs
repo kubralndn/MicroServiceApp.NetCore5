823ed8f8eae20bb241d51b3e61204fea7bb0827d
Kübra Bilgin <bilgin.kbra@gmail.com>
2021-01-13 22:52:38 +0300

    Basket microservice developed

---
+++ Basket.API/Data/BasketContext.cs
@@ -0,0 +1,21 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Data
+{
+    public class BasketContext : IBasketContext
+    {
+        private readonly ConnectionMultiplexer _redisConnection;
+
+        public BasketContext(ConnectionMultiplexer redisConnection)
+        {
+            _redisConnection = redisConnection;
+            Redis = redisConnection.GetDatabase();
+        }
+
+        public IDatabase Redis { get; }
+    }
+}
---
+++ Basket.API/Data/Intefaces/IBasketContext.cs
@@ -0,0 +1,16 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Data.Intefaces
+{
+    public interface IBasketContext
+    {
+        public interface IBasketContext
+        {
+            IDatabase Redis { get; }
+        }
+    }
+}
